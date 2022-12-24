e432a310c8d1d5d37d70be00f836719a225b213b
uttej <uttej@Uttej.localdomain>
2022-12-24 19:08:32 +0800

    adding starting project

---
+++ script.js
@@ -0,0 +1,10 @@
+var button = document.getElementsByTagName("button")[0];
+
+button.addEventListener("click", function() {
+    console.log("Clicked!!!");
+});
+
+button.addEventListener("mouseenter", function() {
+    console.log("Entered!!!");
+});
+
